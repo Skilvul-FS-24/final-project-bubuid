def925b782d63e4e4ec974507986624a7a57a30f
Akbar-aryansyah <akbar.aryansyah88@gmail.com>
2023-11-15 17:06:59 +0700

    footer siap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import Footer from "./components/footer/footer";
-import Navbar from "./components/navbar/navbar";
+
 
 function App() {
   return (
     <>
-      <Footer/>
+  
     </>
   );
 }
