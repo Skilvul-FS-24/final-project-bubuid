5d5f72005d9e93fdae9cb0c263ef9f58cba9a76a
Akbar-aryansyah <akbar.aryansyah88@gmail.com>
2023-11-15 17:02:50 +0700

    footerd done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Footer from "./components/footer/footer";
 import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Footer/>
     </>
   );
 }
