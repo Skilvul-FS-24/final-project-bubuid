e8f9aa0e26ebdecf63a4d3275e93ac4a326dc36a
bagasm24 <mbagas368@gamil.com>
2023-11-20 20:31:17 +0700

    Halaman login dan baca artikel done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Login from "./components/login/login";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>    
-
+      <Login/>
     </>
   );
 }
