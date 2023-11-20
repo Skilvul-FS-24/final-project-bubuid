87b251e9771fe2cfc05ce8362c4396a6d42440f9
bagasm24 <mbagas368@gamil.com>
2023-11-20 15:25:32 +0700

    buat modal untuk form konseling

--- src/components/konseling/detailpsikolog.jsx
+++ src/components/konseling/detailpsikolog.jsx
@@ -13,11 +13,141 @@ function DetailPsikolog() {
         </div>
       </div>
       <div class="flex flex-col md:flex-row justify-center p-10 gap-9">
-        <div class="md:basis-1/5 text-white font-poppins md:grid  md:place-content-between">
+        <div class="md:basis-1/5 font-poppins md:grid  md:place-content-between">
           <img src="/src/assets/physical.png" alt="" className="w-full" />
-          <button className="w-full bg-[#54BAB9] rounded-xl text-sm p-1">
+          {/* <button className="w-full bg-[#54BAB9] rounded-xl text-sm p-1">
+            Konsultasi Sekarang
+          </button> */}
+          {/* You can open the modal using document.getElementById('ID').showModal() method */}
+          <button
+            className="text-white bg-[#54BAB9] p-1 rounded-xl w-full hover:bg-[#9fe4e3]"
+            onClick={() => document.getElementById("my_modal_3").showModal()}
+          >
             Konsultasi Sekarang
           </button>
+          <dialog id="my_modal_3" className="modal">
+            <div className="modal-box">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <form className="mx-auto mt-10">
+                <div className="mb-4">
+                  <label
+                    htmlFor="name"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Nama
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="border rounded w-full py-2 px-3"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="border rounded w-full py-2 px-3"
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="phoneNumber"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    No. HP
+                  </label>
+                  <input
+                    type="tel"
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    className="border rounded w-full py-2 px-3"
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="school"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Asal Sekolah
+                  </label>
+                  <input
+                    type="text"
+                    id="school"
+                    name="school"
+                    className="border rounded w-full py-2 px-3"
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="onlineOffline"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Pilihan Online/Offline
+                  </label>
+                  <select
+                    id="onlineOffline"
+                    name="onlineOffline"
+                    className="border rounded md:w-full py-2 px-3"
+                  >
+                    <option value="online">Online</option>
+                    <option value="offline">Offline</option>
+                  </select>
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="date"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Tanggal
+                  </label>
+                  <input
+                    type="date"
+                    id="date"
+                    name="date"
+                    className="border rounded w-full py-2 px-3"
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="complaint"
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                  >
+                    Keluhan Anda
+                  </label>
+                  <textarea
+                    id="complaint"
+                    name="complaint"
+                    className="border rounded w-full py-2 px-3"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="text-white bg-[#54BAB9] p-1 rounded-xl w-full hover:bg-[#9fe4e3]"
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+          </dialog>
         </div>
         <div class="md:basis-1/2 p-10 bg-[#54BAB9] rounded-xl font-poppins">
           <h1 className="text-xl font-bold">Muhamad Bagas M</h1>
